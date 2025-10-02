86a6972852127e55c21fee0fcf4a1e69648cfabd
hebcodee <herberth.ferreira6@gmail.com>
2025-10-01 21:26:17 -0300

    Describe blocks, It blocks, Interacting with elements, Getting elements, Command Chaining & assertions, beforeEach, Single test and Custom Commands

--- cypress/e2e/fundamentals.cy.js
+++ cypress/e2e/fundamentals.cy.js
@@ -1,5 +1,15 @@
 describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+  beforeEach(() => {
+  cy.visit('/fundamentals')
+  })
+  it('Contains correct header text', () => {
+    cy.getDataTest('fundamentals-header').should('contain.text', 'Testing Fundamentals')
+  })
+  it('Accordion works correctly', () => {
+    cy.contains(/Your tests will exist in a describe block/i).should('not.be.visible')
+    cy.get('[data-test="accordion-item-1"] div[role="button"]').click()
+    cy.contains(/Your tests will exist in a describe block/i).should('be.visible')
+    cy.get('[data-test="accordion-item-1"] div[role="button"]').click()
+    cy.contains(/Your tests will exist in a describe block/i).should('not.be.visible')
   })
 })
