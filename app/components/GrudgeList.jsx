a3460f2245f444ca40556efdef10afda123183ff
hebcodee <herberth.ferreira6@gmail.com>
2025-10-01 21:27:11 -0300

    Multi-page testing, Intercepts, Helpful methods

--- app/components/GrudgeList.jsx
+++ app/components/GrudgeList.jsx
@@ -48,12 +48,13 @@ export default function GrudgeList(){
             value={inputValue}
             onChange={e => setInputValue(e.target.value)}
             style={{backgroundColor: 'white'}}
+            data-test="grudge-input"
             />
             </div>
-            <Button onClick={addGrudge}>
+            <Button data-test="add-grudge-button" onClick={addGrudge}>
                 Add Grudge
             </Button>
-            <ul style={{color: 'white', listStyleType: 'none'}}>
+            <ul data-test="grudge-list" style={{color: 'white', listStyleType: 'none'}}>
                 {
                     grudges.length > 0 && (
                         grudges.map((g) => {
@@ -62,7 +63,7 @@ export default function GrudgeList(){
                                 <span>
                                 {g.text}
                                 </span>
-                                <Button onClick={() => deleteGrudge(g)}>
+                                <Button data-test="remove-grudge-button" onClick={() => deleteGrudge(g)}>
                                     X
                                 </Button> 
                             </li>
@@ -71,7 +72,7 @@ export default function GrudgeList(){
                 }
             </ul>
             {
-                grudges.length > 0 && <Button onClick={clearGrudges}>Clear</Button>
+                grudges.length > 0 && <Button data-test="clear-button" onClick={clearGrudges}>Clear</Button>
             }
         </div>
     )
